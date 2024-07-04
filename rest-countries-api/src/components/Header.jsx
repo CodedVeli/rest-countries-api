42dae42e16a4a7bd2f157b7296196c4a50b613b0
Eric Kibuchi <ericgithaiga007@gmail.com>
2024-07-05 01:10:48 +0300

    filter region by search & filter search by region addition

--- rest-countries-api/src/components/Header.jsx
+++ rest-countries-api/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { MdDarkMode } from "react-icons/md";
+import { CiLight } from "react-icons/ci";
 import { useDarkMode } from "../context/UseDarkMode";
 
 function Header() {
@@ -9,7 +10,7 @@ function Header() {
     <section className="  fixed z-50  left-0 top-0 w-full  bg-White  flex justify-between pt-4 pb-4 pl-10 pr-10 text-Dark-Blue shadow-xl dark:bg-Dark-Blue-Elements">
       <Link to="/"><h1 className=" font-extrabold lg:text-2xl md:text-xl text-lg dark:text-White">Where in the world?</h1></Link>
         <button onClick={() => darkModeHandler()} className=" flex space-x-1 dark:text-White">
-          {darkMode ? <MdDarkMode className=" mt-1 size-5 dark:text-White"/> : <MdDarkMode className=" mt-1 size-5 dark:text-White"/>}
+          {darkMode ? <CiLight className=" mt-1 size-5 dark:text-White"/> : <MdDarkMode className=" mt-1 size-5 dark:text-White"/>}
           <h2>Dark Mode</h2></button>
 
     </section>
