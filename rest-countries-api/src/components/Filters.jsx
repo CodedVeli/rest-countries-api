fa80d264ab8ea9924a635b05268831e4a3bb16e1
Eric Kibuchi <ericgithaiga007@gmail.com>
2024-07-05 00:53:08 +0300

    filter region by search & filter search by region addition

--- rest-countries-api/src/components/Filters.jsx
+++ rest-countries-api/src/components/Filters.jsx
@@ -13,8 +13,7 @@ function Filters({setRegion}) {
   <option value="">Select Continent</option>
   <option value="Africa">Africa</option>
   <option value="Asia">Asia</option>
-  <option value="North America">North America</option>
-  <option value="South America">South America</option>
+  <option value="Americas"> Americas</option>
   <option value="Oceania">Oceania</option>
   <option value="Europe">Europe</option>
   <option value="Asia">Asia</option>
