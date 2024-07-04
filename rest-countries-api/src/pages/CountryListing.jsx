6291eed2ea591a2b1bec37f5063d20b0ef8ddab7
Eric Kibuchi <ericgithaiga007@gmail.com>
2024-07-04 23:56:54 +0300

    filter region by search & filter search by region addition

--- rest-countries-api/src/pages/CountryListing.jsx
+++ rest-countries-api/src/pages/CountryListing.jsx
@@ -2,37 +2,46 @@
 import CountryCard from '../components/CountryCard'
 import { useGetAllCountriesQuery } from '../rtk/apis/CountryApi'
 import Skeleton from '../components/Skeleton'
-import { useGetCountryByNameQuery } from '../rtk/apis/CountryApi'
 import { useGetCountryByRegionQuery } from '../rtk/apis/CountryApi'
 function CountryListing({searchTerm,region}) {
   const { data = [], error, isLoading } = useGetAllCountriesQuery()
-  const { data: countryData = [], isLoadingSearch, errorSearch } = useGetCountryByNameQuery(searchTerm)  
   const { data: dataRegion = [], isLoadingRegion, errorRegion} = useGetCountryByRegionQuery(region);
 
+  let filteredCountries = data;
+  if (searchTerm ) {
+    filteredCountries = filteredCountries.filter(country =>
+      country.name.common.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  }
+
+  if (region) {
+    filteredCountries = filteredCountries.filter(country => country.region === region);
+  }
+
   let countryList;
-  if (searchTerm) {
-    countryList =  countryData.map((country) => (
-      <CountryCard key={country.name.common} {...country} />
-    ))    
-  }  
+  if (filteredCountries) {
+    countryList = filteredCountries.length > 0 ? 
+        filteredCountries.map((country) => <CountryCard key={country.name.common} {...country} />) 
+        : (<p>Not found</p>);
+}
   else if (region) {
     countryList = dataRegion.map((country) => (
       <CountryCard key={country.name.common} {...country} />
     ))    
   }
-  else if (isLoading || isLoadingSearch || isLoadingRegion) {
+  else if (isLoading ||  isLoadingRegion) {
     countryList = <Skeleton  className=' h-[350px] w-full max-w-[15rem]' count={8}/>
   }
-  else if (error || errorSearch || errorRegion) {
+  else if (error ||  errorRegion) {
     countryList = <div>Error: {error.message}</div>
   }
    else {
    countryList = data.map((country) => (
     <CountryCard key={country.name.common} {...country} />
   ))    
-}
+} 
   return (
-    <div className=" bg-White  grid grid-cols-1 md:grid-cols-4 mx-auto lg:gap-10 gap-5 dark:bg-Very-Bark-Blue-bg" >
+    <div className=" bg-Light-Gray  grid grid-cols-1 md:grid-cols-4 mx-auto lg:gap-10 gap-5 dark:bg-Very-Bark-Blue-bg" >
         {countryList}            
             
         </div>
